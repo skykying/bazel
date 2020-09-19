a099440fe528f8a369e569b2ee8d0eb28611e799
skykying <skykying@163.com>
2020-09-19 21:30:06 +0800

    add example headers

--- example/src/main.cc
+++ example/src/main.cc
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "xenctrl.h"
+#include "xen/io/xenbus.h"
+#include "libserial/SerialPort.h"
+#include "python2.7/Python.h"
+#include <boost/optional.hpp>
 
 int main() {
     std::cout << "Hello, world!\n";
 }
+
